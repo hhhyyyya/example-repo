c5538afc2d17489bdff02eb3b1f84c514d87b160
hhhyyyya <126933147+hhhyyyya@users.noreply.github.com>
2023-03-04 11:47:14 +0800

    Update Solution.java

---
+++ 2023-associate-recruitment-technical-main/Q2/Solution.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {
+        List<String> res = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        scanner.nextLine();
+        while (scanner.hasNextLine()) {
+            String[] strs = new String[3];
+            strs = scanner.nextLine().split(" ");
+            if (strs[1] == "false") {
+                res.add(strs[2]);
+            }
+        }
+        if (res.isEmpty()) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+        for(String name:res) {
+            System.out.println(name);
+        }
+    }
+}
