3143ac8e5c1838d2f41d81f571a3b4d78ebb10a3
hhhyyyya <126933147+hhhyyyya@users.noreply.github.com>
2023-03-04 12:43:22 +0800

    Update solution.sql

--- 2023-associate-recruitment-technical-main/Q3/solution.sql
+++ 2023-associate-recruitment-technical-main/Q3/solution.sql
@@ -1,1 +1,5 @@
-SELECT COUNT(*) from  
+select o.owner_id, o.owner_name, count(distinct ca.category_id) as count 
+from owner as o, article as a, category_article_mapping as ca 
+where a.owner_id = o.owner_id and a.article_id = ca.article_id 
+group by o.owner_id 
+order by count desc;
