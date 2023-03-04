15d9a140b21e8ef3afa6fd886865ada6c914f55a
hhhyyyya <126933147+hhhyyyya@users.noreply.github.com>
2023-03-04 11:10:20 +0800

    Add files via upload

---
+++ 2023-associate-recruitment-technical-main/Q1/Solution.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Solution {
+    Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        String[] strIn1 = str.trim().split(" ");  // 以空格分割
+        int n = sc.nextInt();
+        String[] strIn2 = str.trim().split(" ");
+        System.out.println(m, n);
+    }
+}
